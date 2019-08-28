666b1004c26b1b4ccc9ee916909bf2c5726e04e0
Jean Santos <jeancarlosaps@yahoo.com.br>
2019-08-28 14:48:59 -0300

    Created project
    * Created project;
    * Created LaunchScreen and set constraints;
    * Added assets files.

---
+++ iOSRecruitingBrazil/iOSRecruitingBrazil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSRecruitingBrazil
+//
+//  Created by Jean Carlos Antonio Pereira dos Santos on 28/08/19.
+//  Copyright © 2019 Jean Carlos Antonio Pereira dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
